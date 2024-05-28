7c11dc9dc3940f205b17cd5f46505c6bae512970
Tarun Jindal <tarun.jindal112@gmail.com>
2024-05-28 11:48:10 +0530

    added tests part 2

--- tests/conftest.py
+++ tests/conftest.py
@@ -54,6 +54,18 @@ def test_user(client):
 
 
 @pytest.fixture
+def test_user2(client):
+    user_data = {"email": "tarun123@gmail.com",
+                  "password": "password123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
+
+
+@pytest.fixture
 def token(test_user):
     return create_access_token({"user_id": test_user["id"]})
 
@@ -69,7 +81,7 @@ def authorized_client(client, token):
 
 
 @pytest.fixture
-def test_posts(test_user, session):
+def test_posts(test_user, session, test_user2):
     posts_data = [{
         "title": "first title",
         "content": "first content",
@@ -82,6 +94,10 @@ def test_posts(test_user, session):
         "title": "3rd title",
         "content": "3rd content",
         "owner_id": test_user["id"]
+    },{
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user2["id"]
     }]
 
     def create_post_model(post: dict):
--- tests/test_posts.py
+++ tests/test_posts.py
@@ -56,3 +56,75 @@ def test_create_post(authorized_client, test_user, test_posts, title, content, p
     assert created_post.published == published
     assert created_post.owner_id == test_user["id"]
 
+def test_create_post_default_published_true(authorized_client, test_user, test_posts):
+    res = authorized_client.post("/posts", json={"title": "arbitrary title", "content": "aashdhdbh"})
+    created_post = schemas.Post(**res.json())
+    assert res.status_code == 201
+    assert created_post.title == "arbitrary title"
+    assert created_post.content == "aashdhdbh"
+    assert created_post.published == True
+    assert created_post.owner_id == test_user["id"]
+
+
+def test_unauthorized_create_post_default_published_true(client, test_user, test_posts):
+    res = client.post("/posts", json={"title": "arbitrary title", "content": "aashdhdbh"})
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_delete_post(client, test_user, test_posts):
+    res = client.delete(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_delete_post_success(authorized_client, test_user, test_posts):
+    res = authorized_client.delete(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 204
+
+
+def test_delete_post_non_exist(authorized_client, test_user, test_posts):
+    res = authorized_client.delete(f"/posts/8000000")
+    assert res.status_code == 404
+
+
+def test_delete_other_user_post(authorized_client, test_user, test_posts):
+    res = authorized_client.delete(f"/posts/{test_posts[3].id}")
+    assert res.status_code == 403
+
+
+def test_update_post(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[0].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[0].id}", json=data)
+    updated_post = schemas.Post(**res.json())
+    assert res.status_code == 200
+    assert updated_post.title == data["title"]
+    assert updated_post.content == data["content"]
+
+
+def test_update_other_user_post(authorized_client, test_user, test_user2, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[3].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[3].id}", json=data)
+    assert res.status_code == 403
+
+
+def test_unauthorized_user_update_post(client, test_user, test_posts):
+    res = client.put(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_update_post_non_exist(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[3].id
+    }
+    res = authorized_client.put(f"/posts/8000000", json=data)
+    assert res.status_code == 404
+
